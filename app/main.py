039f67c66033453445ffe91ec7fbf54a180a6b4c
Lam Van <tienlam2001@gmail.com>
2022-08-01 17:39:08 -0400

    de

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify,request
 import json
-import os
+
+
 app = Flask(__name__)
-port = int(os.environ.get('PORT', 5000))
 @app.route("/books")
 def api():
-    with open('../data.json', 'r') as file:
+    with open('data.json', 'r') as file:
         data = json.load(file)
         return jsonify(data)
 
@@ -20,10 +20,10 @@ def addBook():
     except KeyError:
         print("Can't find value")
     else:
-        with open('../data.json', 'r') as file:
+        with open('data.json', 'r') as file:
             data = json.load(file)
             updateFile = data
-            with open('../data.json', 'w') as file2:
+            with open('data.json', 'w') as file2:
                 updateFile[bookName] ={"url": urlLink,"description":ideas,"Idea":[]}
                 data= updateFile
                 json.dump(data, file2, indent=4)
@@ -33,7 +33,7 @@ def addBook():
 
 @app.route("/")
 def main():
-    with open('../data.json', 'r') as file:
+    with open('data.json', 'r') as file:
         data = json.load(file)
         key =list(data.keys())
         length = len(data.keys())
@@ -43,7 +43,7 @@ def main():
 
 @app.route("/book/<name>")
 def home(name):
-    with open('../data.json', 'r') as file:
+    with open('data.json', 'r') as file:
         data = json.load(file)
         return render_template("index.html",datas=data[name]['Idea'],line=name,image=data[name]['url'])
 
@@ -52,10 +52,10 @@ def home(name):
 @app.route('/handle/<de>',methods=['get','post'])
 def handleInput(de):
     input = request.form['idea']
-    with open('../data.json', 'r') as file:
+    with open('data.json', 'r') as file:
         data = json.load(file)
         updateFile = data[de]['Idea']
-        with open('../data.json', 'w') as file2:
+        with open('data.json', 'w') as file2:
             updateFile.append(input)
             data[de]['Idea'] = updateFile
             json.dump(data, file2, indent=4)
@@ -68,6 +68,6 @@ def handleInput(de):
 
 @app.route("/data")
 def uploadData():
-    with open('../data.json', 'r') as file:
+    with open('data.json', 'r') as file:
         data = json.load(file)
         return jsonify(data)
